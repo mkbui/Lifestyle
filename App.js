cd86285afbf590eb62779f52f2226aea8302cf3a
KietB <kiet.buibui@hcmut.edu.vn>
2020-06-03 22:02:41 +0700

    Restructure folders

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 /* To be used to import settings for the whole app theme */
 //import { StyleProvider } from "native-base";
 
-import AppNavigation from './components/AppNavigation';
+import AppNavigation from './src/components/AppNavigation';
 
 /* Main function contains Navigator defined in components */
 export default class App extends React.Component {
