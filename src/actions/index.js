801752feda340d6eaa66152e59b69187e142ab81
KietB <kiet.buibui@hcmut.edu.vn>
2020-06-06 21:22:31 +0700

    Add Display Screen + Initialize Reducers basis

---
+++ src/actions/index.js
@@ -0,0 +1,33 @@
+const SET_VIEW_FILTER = 'SET_VIEW_FILTER'
+const ADD_FOOD = 'ADD_FOOD'
+const REMOVE_FOOD = 'REMOVE_FOOD'
+
+export const ViewFilters = {
+  VIEW_ALL: 'VIEW_ALL',
+  VIEW_EGGS: 'VIEW_EGGS',
+}
+
+/* action creators */
+
+/*
+export const actionCreators = {
+  add_food: food => {
+    return {type: ADD_FOOD, food}
+  },
+  remove_food: id => {
+    return {type: REMOVE_FOOD, id};
+  },
+}
+*/
+
+export function addFood(food){
+  return { type: ADD_FOOD, food };
+}
+
+export function removeFood(id){
+  return { type: REMOVE_FOOD, id };
+}
+
+export function setViewFilters(filter) {
+  return { type: SET_VIEW_FILTER, filter}
+}
