2498a2c8a960d4429b42e19386884b0656072cf5
Khôi Vũ <vuhoanganhkhoi11@gmail.com>
2020-07-03 22:44:55 +0700

    Add activity's name modification feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,6 +9,9 @@ export const CREATE_NEW_DAILY = 'CREATE_NEW_DAILY'
 export const ADD_ACTIVITY = 'ADD_ACTIVITY'
 export const REMOVE_ACTIVITY = 'REMOVE_ACTIVITY'
 export const ACTIVATE_ACTIVITY = 'ACTIVATE_ACTIVITY'
+export const MODIFY_ACTIVITY_NAME = 'MODIFY_ACTIVITY_NAME'
+export const MODIFY_ACTIVITY_TIME = 'MODIFY_ACTIVITY_TIME'
+export const MODIFY_ACTIVITY_REPEAT = 'MODIFY_ACTIVITY_REPEAT'
 export const ADD_INCOME_RECORD = 'ADD_INCOME_RECORD'
 export const ADD_EXPENSE_RECORD = 'ADD_EXPENSE_RECORD'
 export const ADD_CONSUME_RECORD = 'ADD_FITNESS_RECORD'
@@ -77,6 +80,18 @@ export function removeActivity(id){
 export function activateActivity(id){
   return { type: ACTIVATE_ACTIVITY, id };
 }
+
+export function modifyActivityName(id, name){
+  return { type: MODIFY_ACTIVITY_NAME, id, name };
+}
+
+// export function modifyActivityTime(id, hour, min){
+//   return { type: MODIFY_ACTIVITY_TIME, id, hour, min };
+// }
+
+// export function modifyActivityRepeat(id, repeat){
+//   return { type: MODIFY_ACTIVITY_REPEAT, id, repeat };
+// }
 
 export function addIncomeRecord(iRecord) {
   return { type: ADD_INCOME_RECORD, iRecord}
